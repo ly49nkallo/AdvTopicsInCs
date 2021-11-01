9e82c03e46d4f62d4a3c2d08dcf30727629a800c
ly49nkallo <tyabrennan@gmail.com>
2021-11-01 12:17:28 -0700

    r

---
+++ towersOfHanoi/Node.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Node:
+    def __init__(self, index, state, parentNode = None, numOfTowers=3, numOfDisks=3):
+        self.index = index
+        self.state = state
+        self.parentNode = parentNode
+        assert isinstance(self.state, np.ndarray)
+        assert np.shape(self.state) == (numOfDisks,numOfTowers)
+        assert isinstance(self.index, int)
+    
+    def __repr__(self):
+        return "node: " + self.index + " state: " + repr(self.state)
+
+    def isValidState(self) -> bool: pass
+
+    def getNeighbors(self) -> set: pass
+
+    
