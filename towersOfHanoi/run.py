9b30fccb11ecf79ddcd843ac16acfa9ce29f3661
ly49nkallo <tyabrennan@gmail.com>
2021-11-05 13:22:03 -0700

    r

--- towersOfHanoi/run.py
+++ towersOfHanoi/run.py
@@ -5,10 +5,10 @@ from argparse import ArgumentParser
 def main():
     parser = ArgumentParser(description='Solve towers of hanoi',
                             epilog="Code written by Ty Brennan")
-    parser.add_argument('-t', '--towers', help='Number of towers', action='store', default=3)
-    parser.add_argument('-d', '--disks', help="Number of disks per tower", action='store', default=3)
+    parser.add_argument('-t', '--towers', help='Number of towers', action='store', default=3, type=int)
+    parser.add_argument('-d', '--disks', help="Number of disks per tower", action='store', default=3, type=int)
     args = parser.parse_args()
-    startingNode = Node(numOfDisks=args.towers,numOfTowers=args.disks) 
+    startingNode = Node(numOfDisks=args.disks,numOfTowers=args.towers) 
     graph = Graph(startingNode)
     for node in graph.BFS():
         print(node)
